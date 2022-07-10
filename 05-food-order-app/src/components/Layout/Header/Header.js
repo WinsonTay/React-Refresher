720ee0df9cd86e8d00f23178a590b46e65d5b2d2
Winson Tay <weeshen90@gmail.com>
2022-07-10 23:16:33 +0800

    added food order components

---
+++ 05-food-order-app/src/components/Layout/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import mealsImage from "../../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css"
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton>Cart</HeaderCartButton>
+      </header>
+      <div className={classes['main-image']}>
+        <img alt="A table full of delicious food !" src={mealsImage} />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
