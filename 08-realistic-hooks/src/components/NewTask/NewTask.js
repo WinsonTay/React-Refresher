aae0859231c82203144955490c5eca120cccf625
Winson Tay <weeshen90@gmail.com>
2022-07-17 21:51:46 +0800

    feature: custom hooks

---
+++ 08-realistic-hooks/src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+//import { } from "react";
+import useHttp from "../../hooks/use-http";
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createdTask = (taskText,taskData) =>{
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
+
+  const enterTaskHandler = async (taskText) => {
+      sendTaskRequest({
+        url: "https://react-test-78642-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+        method:"POST",
+        headers:{'Content-Type':'application/json'},
+        body:{text:taskText}
+      }, createdTask.bind(null,taskText))
+  };
+
+  return (
+    <Section>
+      <TaskForm
+        onEnterTask={enterTaskHandler}
+        loading={isLoading}
+      />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
